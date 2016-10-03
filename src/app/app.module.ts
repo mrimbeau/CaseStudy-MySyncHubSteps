e82c036db8e8fc90fccf1a6aece0603d843b56e3
MRimbeaux <michel.rimbeaux@gmx.ch>
2016-10-03 18:53:22 +0200

    Initial version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {MaterialModule} from '@angular/material';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,8 @@
+import {Dashboard, DashboardComponent} from './dashboard.component';
+import {NgModule} from "@angular/core";
+
+@NgModule ({
+  declarations: [DashboardComponent],
+  exports: [DashboardComponent]
+})
+export class DashboardModule {};
